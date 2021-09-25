71764ec2feb5c1dfec3764ac61ba606a7b4c2373
Burning Magyk <magyk81@gmail.com>
2021-09-25 14:09:47 -0600

    Started tile and island scripts

---
+++ PirateIslands/Assets/Map/Tile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum Type { LAND, WATER, WOOD, TREASURE }
+
+    [SerializeField]
+    private Island island;
+
+    [SerializeField]
+    private Type tileType;
+
+    [SerializeField]
+    bool becomesBridge, isTree;
+
+    public void BecomeBridge()
+    {
+        if (becomesBridge) tileType = Type.WOOD;
+    }
+    public void Timber()
+    {
+        if (isTree) tileType = Type.LAND;
+    }
+
+    public bool isInside(GenericPlayerInterface player)
+    {
+        return false;
+    }
+
+    public bool CanMoveHere(GenericPlayerInterface player)
+    {
+        if (tileType == Type.WOOD)
+        {
+            if (!becomesBridge) return false;
+            else if (player.gameObject.GetComponent<PlayerMonster>() != null)
+                return false;
+        }
+        if (tileType == Type.TREASURE) return false;
+        return true;
+    }
+}
