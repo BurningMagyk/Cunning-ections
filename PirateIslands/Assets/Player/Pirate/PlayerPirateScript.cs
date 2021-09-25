9c66607a5ee7692894bcf03d20b6b473ba167f92
Reece Broughton <DragonAxe@users.noreply.github.com>
2021-09-25 13:35:49 -0600

    Refactored Player/Pirate/Player to Player/Pirate/PlayerPirate Fixed GetSpeed method.

--- PirateIslands/Assets/Player/Pirate/PlayerPirateScript.cs
+++ PirateIslands/Assets/Player/Pirate/PlayerPirateScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerScript : GenericPlayerInterface
+public class PlayerPirateScript : GenericPlayerInterface
 {
     Vector3 rightMove = new Vector3(1, 0, 0);
     Vector3 leftMove = new Vector3(-1, 0, 0);
@@ -10,6 +10,7 @@ public class playerScript : GenericPlayerInterface
     Vector3 upMove = new Vector3(0, 1, 0);
 
     Vector3 desiredDirection = new Vector3();
+    float speedMultiplier = 0.005f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +21,22 @@ public class playerScript : GenericPlayerInterface
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Transform>().localPosition += GetDesiredDirection() * speedMultiplier;
+    }
+    
+    // used for the camera
+    override public Vector3 GetSpeed(){
+        Vector3 desiredDirection = GetDesiredDirection(); 
+        return desiredDirection * speedMultiplier;
+    }
+
+    Vector3 GetDesiredDirection()
+    {
         desiredDirection += (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) * rightMove;
         desiredDirection += (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0) * leftMove;
         desiredDirection += (Input.GetKey(KeyCode.DownArrow) ? 1 : 0) * downMove;
         desiredDirection += (Input.GetKey(KeyCode.UpArrow) ? 1 : 0) * upMove;
         desiredDirection = desiredDirection.normalized;
-
-        GetComponent<Transform>().localPosition += desiredDirection * 0.005f;
-    }
-    
-    // used for the camera
-    override public Vector3 GetSpeed(){
         return desiredDirection;
     }
 }
