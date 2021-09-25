bc783b39c3f78361bb4fa8d56c6dfacb85d05340
Eric Hillmeyer <ehillmeyer1@unm.edu>
2021-09-25 16:54:21 -0600

    Added box colliders to player and tree prefabs that can be worked with to check overlaps.

---
+++ PirateIslands/Assets/Tree/Tree.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public static Tree[] All;
+    // Start is called before the first frame update
+    void Start()
+    {
+                if (All == null)
+        {
+            GameObject[] objs =  GameObject.FindGameObjectsWithTag("Tree");
+            All = new Tree[objs.Length];
+            for (int i = 0; i < objs.Length; i++)
+            {
+                All[i] = objs[i].GetComponent<Tree>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
